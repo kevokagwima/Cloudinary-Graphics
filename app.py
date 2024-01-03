9fe6f03499efe23fd95d89239bf6f1702b4e3e0e
kevin <you@example.com>
2024-01-03 10:07:36 +0300

    Updated Files

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def delete_media(parent_folder, folder_name):
   return redirect(url_for('home'))
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port="5000", host="0.0.0.0")
